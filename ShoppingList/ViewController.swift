6fadd72655615b56141240ade5f202276942e78a
Paul Richardson <developer@photographerafoot.com>
2021-04-15 08:21:29 +1000

    CHG: add a property to contain shopping list

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+	var shoppingList =  [String]()
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
